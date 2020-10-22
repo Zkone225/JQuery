dc9747e4f029704db62ff0694cef2323f5e6a991
Zkone225 <69790618+Zkone225@users.noreply.github.com>
2020-10-22 14:24:01 +0200

    giov

---
+++ cours-jquery-main/02-selecteurs/script.js
@@ -0,0 +1,17 @@
+// LES SELECTEURS EN jQuery
+
+$(document).ready(function(){
+
+    //cibler un element html
+    // $("p").hide();
+
+    //cibler un element html a partir de son id
+    // $("#test").hide();
+
+    //cibler un element html a partir de sa classe
+    // $(".maClasse").hide();
+
+//cibler un element html specifique
+    $("p:first").hide();
+    $("li:first").hide();
+});
